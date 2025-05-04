034e65bc9be12ddc2899e8e8ddea7d29e42e12da
George Hany <georgehany064@gmail.com>
2025-05-05 01:38:11 +0200

    intermediate commit 2

--- firmware/stm32/Core/Src/main.c
+++ firmware/stm32/Core/Src/main.c
@@ -32,13 +32,12 @@ uint16_t read_ADC(){
 
 int main(void)
 {
-	RCC->APB2ENR |= (1 << 2) | (1 << 3); // CLK PA, PB
-	GPIOA->CRL &= 0;
-	GPIOB->CRL &= 0;
-	GPIOA->CRL |= 0x44444440; // pin A0 analog input
-	ADC_init();
-	GPIO_pinMode(GPIOA, 6, INPUT_PULLUP);
-<<<<<<< HEAD
+//	RCC->APB2ENR |= (1 << 2) | (1 << 3); // CLK PA, PB
+//	GPIOA->CRL &= 0;
+//	GPIOB->CRL &= 0;
+//	GPIOA->CRL |= 0x44444440; // pin A0 analog input
+//	ADC_init();
+//	GPIO_pinMode(GPIOA, 6, INPUT_PULLUP);
 	TIM_initPWM(TIM2, 1, 50);
 //	GPIO_pinMode(GPIOB, 12, OUTPUT);
 //	TIM_initDelay(TIM2, 10);
@@ -58,18 +57,6 @@ int main(void)
 //		TIM_delay(TIM2, 100);
 //		GPIO_digitalWrite(GPIOB, 12, LOW);
 //		TIM_delay(TIM2, 100);
-=======
-	TIM_initPWM(TIM4, 4, 100);
-	while (1)
-	{
-		uint16_t reading = read_ADC();
-		reading = map(reading, 0, 4095, 0, 100);
-		if (GPIO_digitalRead(GPIOA, 6)){
-			TIM_writePWM(TIM4, 4, reading);
-		}if(!GPIO_digitalRead(GPIOA, 6)){
-			TIM_writePWM(TIM4, 4, reading);
-		}
->>>>>>> parent of 121c72b (fixing small syntax error in the delay)
 	}
 
 }
