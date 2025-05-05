ef5ad120fa0032578bd44bd294aef251cb88e697
RealBliTzPro <sheemy21@yahoo.com>
2025-05-05 21:07:07 +0200

    readded millis

--- firmware/stm32/MiddleWares/MCAL/TIMR/TIM_interface.h
+++ firmware/stm32/MiddleWares/MCAL/TIMR/TIM_interface.h
@@ -4,6 +4,7 @@
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx.h"
 #include "../../UTIL/BIT_MATH.h"
+#include <stm32f103xb.h>
 
 void TIM_initPWM(TIM_TypeDef *TIMX, uint8_t channel, float frequency);
 void TIM_writePWM(TIM_TypeDef *TIMX, uint8_t channel, float dutyCycle);
@@ -11,5 +12,6 @@ void TIM_initDelay(TIM_TypeDef *TIMX, uint16_t minTime_ms);
 void TIM_delay(TIM_TypeDef *TIMX, uint32_t delay_ms);
 void TIM_delay_long(TIM_TypeDef *TIMX, uint32_t delay_ms);
 void TIM_callback(TIM_TypeDef *TIMX, float minTimeMs, float time, void (*application)()); // we can change the callback function to be anything
-
+void TIM_initMillis(TIM_TypeDef *TIMX, uint16_t trigTime_ms);
+uint32_t TIM_Millis();
 #endif
