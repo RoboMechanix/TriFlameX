d2e354e9e284b23900599af3822a5f4f92f651dd
RealBliTzPro <sheemy21@yahoo.com>
2025-05-26 22:59:31 +0200

    minor edit

--- firmware/stm32/MiddleWares/MCAL/UART/UART_interface.h
+++ firmware/stm32/MiddleWares/MCAL/UART/UART_interface.h
@@ -1,6 +1,8 @@
 #ifndef UART_INTERFACE_H
 #define UART_INTERFACE_H
-
+void UART_init(int UART_pref_num, int baudrate);
+void UART_send(int UART_pref_num, int data);
+int UART_Recieve(int UART_pref_num);
 
 
 #endif // UART_INTERFACE_H
