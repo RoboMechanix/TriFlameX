97ee96ff8d6b2b08c36f8f014cf6f15df5871378
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-05-26 23:27:10 +0200

    implented publishing's logic for car status

--- firmware/laptop_mainBrain/laptop_pub.py
+++ firmware/laptop_mainBrain/laptop_pub.py
@@ -1,10 +1,35 @@
 import time
 import paho.mqtt.client as mqtt
-from config import MQTT_TOPIC_PUB_BLACK, blackCar_data, MQTT_TOPIC_PUB_RED ,redCar_data, MQTT_TOPIC_PUB_BLUE, blueCar_data
+from config import (
+    MQTT_TOPIC_PUB_BLACK,
+    blackCar_data,
+    MQTT_TOPIC_PUB_RED,
+    redCar_data,
+    MQTT_TOPIC_PUB_BLUE,
+    blueCar_data,
+    isBlueCar_live,
+    isRedCar_live,
+    isBlackCar_live)
+
+firstTime = True
+
+blueCar_prevState = False
+redCar_prevState = False
+blackCar_prevState = False
 
 def on_publish(client, userdata, mid):
     #print("message published")
-    pass
+    global blueCar_prevState, redCar_prevState, blackCar_prevState
+    
+    if firstTime:
+        firstTime = False
+        blueCar_prevState = isBlueCar_live
+        redCar_prevState = isRedCar_live
+        blackCar_prevState = isBlackCar_live
+        print_status()
+    else:
+        check_status()
+    
 
 def publish_message(client):
     while True:
@@ -37,3 +62,23 @@ def pubMsg(msg, topic, client):
         )
     pubMsg.wait_for_publish()
 
+
+def check_status():
+    global blueCar_prevState, redCar_prevState, blackCar_prevState
+    changed = False
+    if blueCar_prevState != isBlueCar_live:
+        blueCar_prevState = isBlueCar_live
+        changed = True
+    if redCar_prevState != isRedCar_live:
+        redCar_prevState = isRedCar_live
+        changed = True
+    if blackCar_prevState != isBlackCar_live:
+        blackCar_prevState = isBlackCar_live
+        changed = True
+    if changed:
+        print_status()
+
+def print_status():   
+    print ("Blue Car is live ✅" if isBlueCar_live else "Blue Car is Dead 🛑") 
+    print ("Red Car is live ✅" if isRedCar_live else "Red Car is Dead 🛑")
+    print ("Black Car is live ✅" if isBlackCar_live else "Black Car is Dead 🛑")
