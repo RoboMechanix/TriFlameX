637b2c54d86c8eac8097b19e6587e295d59e1cbb
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-05-27 00:24:09 +0200

    dvided  the sensor topic  into 3 diff topics

--- firmware/laptop_mainBrain/config.py
+++ firmware/laptop_mainBrain/config.py
@@ -1,7 +1,9 @@
 # === Configuration ===
 MQTT_BROKER = "192.168.0.69"      
 MQTT_PORT = 1883
-MQTT_TOPIC_SUB = "sensor/esp32"
+MQTT_TOPIC_SUB_BLUE = "sensor/bluecar"
+MQTT_TOPIC_SUB_RED = "sensor/redcar"
+MQTT_TOPIC_SUB_BLACK = "sensor/blackcar"
 MQTT_TOPIC_PUB_BLUE = "laptop/commands/bluecar"
 MQTT_TOPIC_PUB_RED = "laptop/commands/redcar"
 MQTT_TOPIC_PUB_BLACK = "laptop/commands/blackcar"
--- firmware/laptop_mainBrain/laptop_sub.py
+++ firmware/laptop_mainBrain/laptop_sub.py
@@ -3,7 +3,9 @@ import time
 import threading
 
 from config import (
-    MQTT_TOPIC_SUB,
+    MQTT_TOPIC_SUB_BLUE,
+    MQTT_TOPIC_SUB_RED,
+    MQTT_TOPIC_SUB_BLACK
 )
 
 import config
@@ -44,7 +46,9 @@ def on_connect(client, userdata, flags, reason_code, properties=None):
     if reason_code == 0:
         print("✅ Connected to MQTT Broker!")
         is_connected = True
-        client.subscribe(MQTT_TOPIC_SUB)
+        client.subscribe(MQTT_TOPIC_SUB_BLUE)
+        client.subscribe(MQTT_TOPIC_SUB_RED)
+        client.subscribe(MQTT_TOPIC_SUB_BLACK)
     else:
         print(f"❌ Failed to connect, return code {reason_code}")
         is_connected = False
@@ -52,23 +56,24 @@ def on_connect(client, userdata, flags, reason_code, properties=None):
 # === Callback when a message is received ===
 def on_message(client, userdata, msg):
      payload = msg.payload.decode('utf-8')
+     topic = msg.topic
 
-     if payload.startswith("ESP32_BlueCar: "):
+     if topic == MQTT_TOPIC_SUB_BLUE:
         data_str = payload.split(": ")[1].strip()  #  "100 cm"
         config.blueCar_data = int(data_str.split()[0])    # "100"
         last_seen["blue"] = time.time()
 
-     elif payload.startswith("ESP32_RedCar: "):
+     elif topic == MQTT_TOPIC_SUB_RED:
         data_str = payload.split(": ")[1].strip()
         config.redCar_data = int(data_str.split()[0])
         last_seen["red"] = time.time()
 
-     elif payload.startswith("ESP32_BlackCar: "):
+     elif topic == MQTT_TOPIC_SUB_BLACK:
         data_str = payload.split(": ")[1].strip()
         config.blackCar_data = int(data_str.split()[0])
         last_seen["black"] = time.time()
 
-     print(f"📩 Received from ESP32 on topic '{msg.topic}': {payload}")
+     #print(f"📩 Received from ESP32 on topic '{msg.topic}': {payload}")
 
 # === Start the monitoring thread ===
 threading.Thread(target=monitor_car_status, daemon=True).start()
