c0ab8ec94f652a33ff7ebb1d4a41c8c794150a71
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-06-01 01:02:30 +0200

    Started implemting MQTT on msg call

--- firmware/laptop_mainBrain/laptop_sub.py
+++ firmware/laptop_mainBrain/laptop_sub.py
@@ -58,6 +58,11 @@ def on_connect(client, userdata, flags, reason_code, properties=None):
 
 # === Callback when a message is received ===
 def on_message(client, userdata, msg):
+    
+    if topic in [MQTT_TOPIC_SUB_BLUE_ROS, MQTT_TOPIC_SUB_RED_ROS, MQTT_TOPIC_SUB_BLACK_ROS]:
+        takeOver_ros(msg)
+        return
+    
     payload = msg.payload.decode('utf-8')
     topic = msg.topic
      
@@ -65,7 +70,7 @@ def on_message(client, userdata, msg):
         value = int(payload)  # parse directly as int
     except ValueError:
         print(f"⚠️ Invalid payload: '{payload}'")
-        return  # Exit early if payload is invalid
+        return  
 
     if topic == MQTT_TOPIC_SUB_BLUE:
         config.blueCar_data = value
@@ -82,3 +87,25 @@ def on_message(client, userdata, msg):
 
 # === Start the monitoring thread ===
 threading.Thread(target=monitor_car_status, daemon=True).start()
+
+def takeOver_ros(msg):
+    payload = msg.payload
+    topic = msg.topic
+    
+    try:
+        value = int.from_bytes(payload, 'big')      # Convert 3 bytes to integer
+    except ValueError:
+        print(f"⚠️ Invalid payload: '{payload}'")
+        return 
+
+    if topic == MQTT_TOPIC_SUB_BLUE_ROS:
+        config.blueCar_data = value
+        last_seen["blue"] = time.time()
+        
+    elif topic == MQTT_TOPIC_SUB_RED_ROS:
+        config.redCar_data = value
+        last_seen["red"] = time.time()
+
+    elif topic == MQTT_TOPIC_SUB_BLACK_ROS:
+        config.blackCar_data = value
+        last_seen["black"] = time.time()
