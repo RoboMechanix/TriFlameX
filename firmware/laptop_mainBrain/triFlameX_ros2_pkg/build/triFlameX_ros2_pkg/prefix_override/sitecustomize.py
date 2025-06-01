f28d78184ff5105feb4cc39fe1d47460a91e76c3
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-06-01 21:53:25 +0200

    Fixed non-convential ros2 pkg name

--- firmware/laptop_mainBrain/triFlameX_ros2_pkg/build/triFlameX_ros2_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mohammedazab/myRepo/TriFlameX/firmware/laptop_mainBrain/triFlameX_ros2_pkg/install/triFlameX_ros2_pkg'
