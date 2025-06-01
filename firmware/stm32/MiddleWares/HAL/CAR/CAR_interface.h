77ca2d5fd9a91b8a559b5ec5c1263ca90556714d
RealBliTzPro <sheemy21@yahoo.com>
2025-06-01 19:09:01 +0200

    redid all of control and added angle control with testing main

--- firmware/stm32/MiddleWares/HAL/CAR/CAR_interface.h
+++ firmware/stm32/MiddleWares/HAL/CAR/CAR_interface.h
@@ -4,11 +4,15 @@
 #include "stm32f103xb.h"
 #include "../../MCAL/TIMR/TIM_interface.h"
 
-void CAR_init(TIM_TypeDef *TimxLeft ,uint8_t ChannelLeft, float FrequencyLeft ,GPIO_TypeDef *DirPin1,uint8_t Dir1pinNumber,TIM_TypeDef *TimxRight ,uint8_t ChannelRight, float FrequencyRight, GPIO_TypeDef *DirPin2,uint8_t Dir2pinNumber) ;
-void CAR_forward(float Speed);
-void CAR_backwards(float Speed);
-void CAR_right(float Speed);
-void CAR_left(float Speed);
+void CAR_init(TIM_TypeDef *TimxLeft ,uint8_t ChannelLeft, float FrequencyLeft,
+              GPIO_TypeDef *DirLeft1, GPIO_TypeDef *DirLeft2, uint8_t DirLeft1Pin, uint8_t DirLeft2Pin,
+              TIM_TypeDef *TimxRight ,uint8_t ChannelRight, float FrequencyRight,
+              GPIO_TypeDef *DirRight1, GPIO_TypeDef *DirRight2, uint8_t DirRight1Pin, uint8_t DirRight2Pin);
+
+void CAR_forward(float rightSpeed , float leftSpeed);
+void CAR_backwards(float rightSpeed , float leftSpeed);
+void CAR_right(float leftSpeed, float rightSpeed) ;
+void CAR_left(float rightSpeed, float leftSpeed);
 void CAR_stop();
 
 #endif
