28eb4fd2516dcbf9173fad86d6bafcc02b91f453
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-06-11 23:28:08 +0200

    made the angle independent

--- firmware/laptop_mainBrain/triflamex_ros2_pkg/triflamex_ros2_pkg/joy_to_cmd_node.py
+++ firmware/laptop_mainBrain/triflamex_ros2_pkg/triflamex_ros2_pkg/joy_to_cmd_node.py
@@ -84,7 +84,7 @@ class JoyToCmd(Node):
         sign = 0 if raw_angle >= 0 else 1
         command = 1 if abs(raw_throttle) > 0.1 or abs(raw_angle) > 0.1 else 0
                 
-        throttle = speed_array[self.index] if command == 1 else 10 
+        throttle = speed_array[self.index] if throttle > 50 else 10 
         angle = angle_array[self.index] if angle > 10 else 90
     
         angle = angle - 90 if (sign) else angle
