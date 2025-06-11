7d7f70048b29735a7bd05b16b8edfc3b1b9c4e8f
Mohammed Azab <mo7ammed3zab@outlook.com>
2025-06-11 10:01:44 +0200

    removed ros2 caches for tracked files

--- firmware/laptop_mainBrain/triflamex_ros2_pkg/build/triflamex_ros2_pkg/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mohammedazab/myRepo/TriFlameX/firmware/laptop_mainBrain/triflamex_ros2_pkg/install/triflamex_ros2_pkg'
